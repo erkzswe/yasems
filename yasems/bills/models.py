e8b82b916050169ea84aa10e4ea9187c6e4ed2bc
Eryk Malczyk <eryk.malczyk@neontri.com>
2024-06-10 09:11:23 +0200

    Add bill and product class for storing information for payments sharing

--- yasems/bills/models.py
+++ yasems/bills/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Bill(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    products = models.ManyToManyField(Product)
+    description = models.TextField(blank=True, null=True)
+    creator = models.ForeignKey('core.User')
+    contributors = models.ManyToManyField('core.User', related_name='contributors', blank=True)
+    
